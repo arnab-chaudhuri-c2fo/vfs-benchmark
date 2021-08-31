8eff83e0f2b74a4357d0c233b5207ce0ef58673e
arnab-chaudhuri-c2fo <87777587+arnab-chaudhuri-c2fo@users.noreply.github.com>
2021-08-30 20:24:24 -0500

    WIP

--- vfs_benchmark_read_test.go
+++ vfs_benchmark_read_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func BenchmarkRead_AWS_File(b *testing.B) {
+func BenchmarkRead_AWS_File_100MB(b *testing.B) {
 
 	for i := 0; i < b.N; i++ {
 		fmt.Printf("Iteration - %v\n", i)
@@ -13,6 +13,19 @@ func BenchmarkRead_AWS_File(b *testing.B) {
 			"c2foupload-test",
 			"/vfs-benchmark/folder2/files/",
 			"100MB_file",
+			&s3Creds)
+	}
+
+}
+
+func BenchmarkRead_AWS_File_1GB(b *testing.B) {
+
+	for i := 0; i < b.N; i++ {
+		fmt.Printf("Iteration - %v\n", i)
+		ReadRemoteFileAWS(
+			"c2foupload-test",
+			"/vfs-benchmark/folder2/files/",
+			"temp_1GB_file",
 			&s3Creds)
 	}
 
